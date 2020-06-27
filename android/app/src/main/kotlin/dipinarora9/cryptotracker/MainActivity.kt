1725bfa571b4f21f3ffcefb51b7e9b8058d99432
dipinarora9 <dipinarora401@gmail.com>
2020-06-27 23:24:52 +0530

    added base structure

---
+++ android/app/src/main/kotlin/dipinarora9/cryptotracker/MainActivity.kt
@@ -0,0 +1,6 @@
+package dipinarora9.cryptotracker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/tracker_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Tracker extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
